5a9a0a25b302d095f6ed025349dad71fbec06afd
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-03-14 17:24:55 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const cor3 = document.querySelector(".three");
 const cor4 = document.querySelector(".four");
 const botaoCores = document.getElementById('button-random-color');
 const colorPalette = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
 
 botaoCores.addEventListener("click", coresAleatorias);
 
@@ -12,4 +13,11 @@ function coresAleatorias() {
     cor3.style.backgroundColor = `rgb(${Math.random() * (250 - 0) + 0}, ${Math.random() * (250 - 0) + 0}, ${Math.random() * (250 - 0) + 0})`;
     cor4.style.backgroundColor = `rgb(${Math.random() * (250 - 0) + 0}, ${Math.random() * (250 - 0) + 0}, ${Math.random() * (250 - 0) + 0})`;   
     localStorage.setItem('colorPalette', colorPalette);
+};
+
+
+for (let index = 0; index < 25; index++) {
+    let pixelDiv = document.createElement('div')
+    pixelDiv.setAttribute('class', 'pixel')
+    pixelBoard.appendChild(pixelDiv);
 };
