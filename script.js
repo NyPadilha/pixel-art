a85a8035bf15ca9ae75faa422a46f2666aee41ab
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-03-14 18:00:54 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const cor1 = document.querySelector(".one");
 const cor2 = document.querySelector(".two");
 const cor3 = document.querySelector(".three");
 const cor4 = document.querySelector(".four");
 const botaoCores = document.getElementById('button-random-color');
 const colorPalette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
+const pixel = document.querySelector('.pixel');
 
 botaoCores.addEventListener("click", coresAleatorias);
 
@@ -21,3 +23,13 @@ for (let index = 0; index < 25; index++) {
     pixelDiv.setAttribute('class', 'pixel')
     pixelBoard.appendChild(pixelDiv);
 };
+
+cor1.classList.add('selected');
+
+colorPalette.addEventListener("click", (event) => {
+    const corSelect = document.querySelector('.selected')
+    corSelect.classList.remove('selected');
+    event.target.classList.add('selected');
+
+});
+
